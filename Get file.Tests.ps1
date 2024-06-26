25ddeb27b7f7f02621ed87d085f77f80758dd5b0
DarkLite1 <BrechtGijbels@gmail.com>
2024-06-26 10:22:47 +0200

    add separate script for Get file

---
+++ Get file.Tests.ps1
@@ -0,0 +1,125 @@
+﻿#Requires -Modules Pester
+#Requires -Version 7
+
+BeforeAll {
+    $testScript = $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+    $testParams = @{
+        Path    = New-Item 'TestDrive:/f' -ItemType Directory
+        Filters = @('*.txt')
+        Recurse = $false
+    }
+}
+Describe 'the mandatory parameters are' {
+    It '<_>' -ForEach @('Path', 'Recurse', 'Filters') {
+        (Get-Command $testScript).Parameters[$_].Attributes.Mandatory |
+        Should -BeTrue
+    }
+}
+Describe 'throw a terminating error when' {
+    It 'Path is not found' {
+        $testNewParams = $testParams.clone()
+        $testNewParams.Path = 'z:\notExisting'
+
+        {
+            & $testScript @testNewParams
+        } |
+        Should -Throw -ExpectedMessage "Folder '$($testNewParams.Path)' not found."
+    }
+}
+Describe 'return an object' {
+    BeforeAll {
+        $testFiles = @(
+            'file1.txt', 'file2.txt', 'file3.doc'
+        ).ForEach(
+            { New-Item -Path "$($testParams.Path)\$_" -ItemType File }
+        )
+
+        $testNewParams = $testParams.clone()
+        $testNewParams.Filters = @('*.txt', '*.xlsx', '*.oops')
+
+        Mock Get-ChildItem {
+            throw 'Oops'
+        } -ParameterFilter {
+            $Filter -eq $testNewParams.Filters[2]
+        }
+
+        $actual = & $testScript @testNewParams
+    }
+    It 'for each filter' {
+        $actual | Should -HaveCount $testNewParams.Filters.Count
+    }
+    Context 'when files are found' {
+        Context 'with property' {
+            It 'Files' {
+                $expected = $testFiles.where(
+                    { $_.extension -eq '.txt' }
+                )
+
+                $actual[0].Files | Should -HaveCount $expected.Count
+
+                foreach ($file in $expected) {
+                    $actual[0].Files.FullName | Should -Contain $file.FullName
+                }
+            }
+            It 'Filter' {
+                $actual[0].Filter | Should -Be $testNewParams.Filters[0]
+            }
+            It 'Recurse' {
+                $actual[0].Recurse | Should -BeN $testNewParams.Recurse
+            }
+            It 'StartTime' {
+                $actual[0].StartTime | Should -BeOfType 'System.DateTime'
+            }
+            It 'EndTime' {
+                $actual[0].EndTime | Should -BeOfType 'System.DateTime'
+            }
+            It 'Error' {
+                $actual[0].Error | Should -BeNullOrEmpty
+            }
+        }
+    }
+    Context 'when no files are found' {
+        Context 'with property' {
+            It 'Files' {
+                $actual[1].Files | Should -BeNullOrEmpty
+            }
+            It 'Filter' {
+                $actual[1].Filter | Should -Be $testNewParams.Filters[1]
+            }
+            It 'Recurse' {
+                $actual[1].Recurse | Should -BeN $testNewParams.Recurse
+            }
+            It 'StartTime' {
+                $actual[1].StartTime | Should -BeOfType 'System.DateTime'
+            }
+            It 'EndTime' {
+                $actual[1].EndTime | Should -BeOfType 'System.DateTime'
+            }
+            It 'Error' {
+                $actual[1].Error | Should -BeNullOrEmpty
+            }
+        }
+    }
+    Context 'when Get-ChildItem fails' {
+        Context 'with property' {
+            It 'Files' {
+                $actual[2].Files | Should -BeNullOrEmpty
+            }
+            It 'Filter' {
+                $actual[2].Filter | Should -Be $testNewParams.Filters[2]
+            }
+            It 'Recurse' {
+                $actual[2].Recurse | Should -BeN $testNewParams.Recurse
+            }
+            It 'StartTime' {
+                $actual[2].StartTime | Should -BeOfType 'System.DateTime'
+            }
+            It 'EndTime' {
+                $actual[2].EndTime | Should -BeOfType 'System.DateTime'
+            }
+            It 'Error' {
+                $actual[2].Error | Should -Be "Failed retrieving files with filter '$($testNewParams.Filters[2])': Oops"
+            }
+        }
+    }
+}
---
+++ Get file.ps1
@@ -0,0 +1,41 @@
+﻿Param (
+    [Parameter(Mandatory)]
+    [String]$Path,
+    [Parameter(Mandatory)]
+    [Boolean]$Recurse,
+    [Parameter(Mandatory)]
+    [String[]]$Filters
+)
+
+if (-not (Test-Path -LiteralPath $Path -PathType 'Container')) {
+    throw "Folder '$Path' not found."
+}
+
+foreach ($filter in $Filters) {
+    try {
+        $result = [PSCustomObject]@{
+            Filter    = $filter
+            Files     = @()
+            StartTime = Get-Date
+            EndTime   = $null
+            Error     = $null
+        }
+
+        $params = @{
+            LiteralPath = $Path
+            Recurse     = $Recurse
+            Filter      = $filter
+            File        = $true
+            ErrorAction = 'Stop'
+        }
+        $result.Files += Get-ChildItem @params
+    }
+    catch {
+        $result.Error = "Failed retrieving files with filter '$filter': $_"
+        $Error.RemoveAt(0)
+    }
+    finally {
+        $result.EndTime = Get-Date
+        $result
+    }
+}
